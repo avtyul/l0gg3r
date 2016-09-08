a30f448ef93d4442272d7f8ba1e55f00b140a1fd
Alexey <avtyul@github.com>
2016-09-09 01:10:45 +0300

    i l0v3 c0pypast3

---
+++ server.js
@@ -0,0 +1,25 @@
+let express = require('express');
+
+let app = express();
+
+const PUBLIC_DIR = __dirname + '/public';
+
+let reqCount = 0;
+
+app.get('/', (req, res) => {
+    // I l0v3 c0pypast3
+    console.log('Request #%d', ++reqCount);
+    console.log('Headers:');
+    console.log(req.headers);
+    console.log('Body:');
+    console.log(req.body);
+    console.log('Query:');
+    console.log(req.query);
+    res.send('fs0ci3ty');
+});
+
+app.use('/', express.static(PUBLIC_DIR));
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log('Hacking started');
+});
